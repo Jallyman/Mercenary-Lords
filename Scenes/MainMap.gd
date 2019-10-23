ca9385d3111c028bad0e452430d4e2e97e94b595
Jasper <you@example.com>
2019-10-23 21:49:30 +0100

    Spawning overlap fix

--- Scenes/MainMap.gd
+++ Scenes/MainMap.gd
@@ -1,7 +1,6 @@
 extends TileMap
 
 # class member variables go here, for example:
-var i = 2
 
 # var b = "textvar"
 
@@ -12,18 +11,28 @@ func _ready():
 
 func _process(delta):
 
-	if(self.has_node("Mob") == true):
-		print_tree()
-		i+= 1
-		print(i)
-		var mob = load("res://scenes/Mob.tscn").instance()
-		var tileMap = get_node("/root/Node/TileMap")
-		mob.set_name("Mob")
-		mob.set_position(Vector2(64,64))
-		tileMap.add_child(mob)
-		i = 0
-	
 	
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
-#	pass
+	pass
+
+
+
+
+
+
+
+
+
+
+# Old Code
+#	if(self.has_node("Mob") == true):
+#		print_tree()
+#		i+= 1
+#		print(i)
+#		var mob = load("res://scenes/Mob.tscn").instance()
+#		var tileMap = get_node("/root/Node/TileMap")
+#		mob.set_name("Mob")
+#		mob.set_position(Vector2(64,64))
+#		tileMap.add_child(mob)
+#		i = 0
--- Scenes/ZombieSpawner.gd
+++ Scenes/ZombieSpawner.gd
@@ -1,42 +1,82 @@
 extends Area2D
 
-# class member variables go here, for example:
 var i = 0
+var j = 0
+var spawner = self
+var extents
+var mobPos
+var mobPos2
 
 func _ready():
-	#var Mob = load("res://scenes/Mob.tscn").instance()
+
+	var spawnerShape = get_node("/root/Node/TileMap/Spawner/SpawnerShape")
+	var spawnerPos = spawner.get_position()
+	extents = spawnerShape.shape.get_extents()
 	
-	#Mob.set_name("ZombieMob")
-	#Mob.set_position(Vector2(64,64))
-		pass
+	pass
 
 func _process(delta):
-
+	
 	if(self.has_node("Mob") == false):
-		
 		i+= 1
-		if(i == 200):
+		
+	if(self.has_node("Mob2") == false):
+		j+= 1
+		
+	if(i == 200):
+		if(self.has_node("Mob") == false):
 			var mob = load("res://scenes/Mob.tscn").instance()
-			var mob2 = load("res://scenes/Mob.tscn").instance()
-			var spawner = self
-			var spawnerShape = get_node("/root/Node/TileMap/Spawner/SpawnerShape")
-			var spawnerPos = spawner.get_position()
-			var extents = spawnerShape.shape.get_extents()
-			var xPos = spawnerPos.x + extents.x*2
-			var yPos = spawnerPos.y + extents.y*2
-			var mobPos1 = Vector2(0 + ((round(rand_range((extents.x*-1)/32,(extents.x)/32))*32)),0 + ((round(rand_range((extents.x*-1)/32,(extents.x)/32))*32)))
-			var mobPos2 = Vector2(0 + ((round(rand_range((extents.x*-1)/32,(extents.x)/32))*32)),0 + ((round(rand_range((extents.x*-1)/32,(extents.x)/32))*32)))
-			#print(xPos)
-			#print(yPos)
-			# var mobPos = Vector2(randi() % (spawnerPos.x + extents.x*2), randi() % (spawnerPos.y + extents.y*2))
+			mobPos = Vector2(0 + ((round(rand_range((extents.x*-1)/32,(extents.x)/32))*32)),0 + ((round(rand_range((extents.x*-1)/32,(extents.x)/32))*32)))
+			print(0 + (rand_range(((extents.x*-1)/32),((extents.x)/32)))*32)
 			
-			# print(extents)
-			print(spawnerPos)
-			mob.set_name("Mob")
-			mob2.set_name("Mob2")
-			mob.set_position(mobPos1)
-			mob2.set_position(mobPos2)
-			spawner.add_child(mob)
-			spawner.add_child(mob2)
-			i = 0
+			if(mobPos2 != mobPos):
+				print("made it 1")
+				mob.set_name("Mob")
+				mob.set_position(mobPos)
+				spawner.add_child(mob)
+				i = 0
+			i-= 1
+		
+	if(j == 600):
+		if(self.has_node("Mob2") == false):
+			var mob2 = load("res://scenes/Mob.tscn").instance()
+			mobPos2 = Vector2(0 + ((round(rand_range((extents.x*-1)/32,(extents.x)/32))*32)),0 + ((round(rand_range((extents.x*-1)/32,(extents.x)/32))*32)))
+
+			
+			if(mobPos != mobPos2):
+				print("made it 2")
+				mob2.set_name("Mob2")
+				mob2.set_position(mobPos2)
+				spawner.add_child(mob2)
+				j = 0
+			j-= 1
+			
 	pass
+
+
+
+
+#if(self.has_node("Mob") == true):
+#				var mob = load("res://scenes/Mob.tscn").instance()
+#				var mob2 = load("res://scenes/Mob.tscn").instance()
+#				var spawner = self
+#				var spawnerShape = get_node("/root/Node/TileMap/Spawner/SpawnerShape")
+#				var spawnerPos = spawner.get_position()
+#				var extents = spawnerShape.shape.get_extents()
+#				var xPos = spawnerPos.x + extents.x*2
+#				var yPos = spawnerPos.y + extents.y*2
+#				var mobPos1 = Vector2(0 + ((round(rand_range((extents.x*-1)/32,(extents.x)/32))*32)),0 + ((round(rand_range((extents.x*-1)/32,(extents.x)/32))*32)))
+#				var mobPos2 = Vector2(0 + ((round(rand_range((extents.x*-1)/32,(extents.x)/32))*32)),0 + ((round(rand_range((extents.x*-1)/32,(extents.x)/32))*32)))
+#				#print(xPos)
+#				#print(yPos)
+#				# var mobPos = Vector2(randi() % (spawnerPos.x + extents.x*2), randi() % (spawnerPos.y + extents.y*2))
+#
+#				# print(extents)
+#				print(spawnerPos)
+#				mob.set_name("Mob")
+#				mob2.set_name("Mob2")
+#				mob.set_position(mobPos1)
+#				mob2.set_position(mobPos2)
+#				spawner.add_child(mob)
+#				spawner.add_child(mob2)
+#				i = 0
