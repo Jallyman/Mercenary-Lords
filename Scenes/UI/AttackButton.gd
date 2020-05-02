9b6e36040c729f8c77f66884693a8d47314b4638
jallyman <jaspergillespie@gmail.com>
2020-05-02 13:22:02 +0100

    new install - hopefully not broken

---
+++ Scenes/UI/AttackButton.gd
@@ -0,0 +1,28 @@
+extends TextureButton
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var mob = null
+
+
+func _on_Attack_button_pressed():
+	
+	var characterGold = get_node("/root/PlayerData").gold
+	var mobGold = get_node("/root/Node/TileMap/Spawner/ZombieMob").gold
+	
+	if(mob.visible):
+		
+		characterGold = characterGold + mobGold
+		mob.hide()
+		mob.queue_free()
+		self.hide()
+		get_node("/root/PlayerData").gold = characterGold
+		
+		print(characterGold)
+		#get_node("AttackButton").queue_free()
+		
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
---
+++ Scenes/UI/StartMenu.gd
@@ -0,0 +1,25 @@
+# StartNebu.gd
+extends Control
+
+# Load the custom images for the mouse cursor
+var point = load("res://images/Cursors/32/iconfinder_katana_blood.png")
+var normal = load("res://images/Cursors/32/iconfinder_katana_479474.png")
+
+func _ready():
+	# Changes only the arrow shape of the cursor
+	# This is similar to changing it in the project settings
+	Input.set_custom_mouse_cursor(normal,0)
+
+	# Changes a specific shape of the cursor (here the IBeam shape)
+	Input.set_custom_mouse_cursor(point,2)
+
+func _on_Start_pressed():
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://scenes/UI/UI.tscn")
+
+
+func _on_Quit_game_pressed():
+	get_tree().quit()
+
+
+
